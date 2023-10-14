e65372ec508718c269bee16570d67e554e0e083e
yamadabruno <bruno_yamada@hotmail.com>
2023-10-13 21:30:33 -0300

    Lista de Pokemon

---
+++ js/poke-api/lista-pokemon.js
@@ -0,0 +1,47 @@
+const pokemonList = document.getElementById('lista-pokemon')
+const loadMoreButton = document.getElementById('carregarMaisBotao')
+
+const maxRecords = 905
+const limit = 10
+let offset = 0
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="cartao-pokemon">
+            <div class="informacoes">
+            <span>${pokemon.name}</span>
+            <span>nº ${pokemon.number}</span>
+            </div>
+
+            <img src="${pokemon.photo}" alt="imagem do bulbasaur" class="gif">
+
+            <ul class="tipos">
+                ${pokemon.types.map((type) => `<li class="tipo ${type}">${type}</li>`).join('')}
+            </ul>
+
+            <p class="descricao">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae, quibusdam?</p>
+
+        </li>
+        `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+
+    const qtdRecordWithNextPage = offset + limit
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    } else {
+    loadPokemonItens(offset, limit)
+    }
+})
